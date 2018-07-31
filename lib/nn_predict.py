210089332b112725a16e30da13ec0ea08c37b074
glenn <glenn@glenns-MacBook.local>
2018-07-31 19:22:05 -0400

    Add dict parsing method to make prediction inputs more understandable

--- lib/nn_predict.py
+++ lib/nn_predict.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 import lib.test_inputs as test_inputs
 
-"""Load model from export_dir, predict on input data, expected output is 2,2,3."""
+"""Load model from export_dir, predict on input data, expected output is 5."""
 export_dir = './tmp/'
 checkpoint_path = tf.train.latest_checkpoint(export_dir)
 saver = tf.train.import_meta_graph(checkpoint_path + ".meta", import_scope=None)
@@ -25,5 +25,42 @@ def rate(arr):
         rating = np.asscalar(output[0])
     return str(rating)
 
+def ratingNumToGrade(rating):
+    if(rating == 10):
+        return "AAA" 
+    elif(rating  == 9):
+        return "AA"
+    elif(rating  == 8):
+        return "A"
+    elif(rating  == 7):
+        return "BBB"
+    elif(rating  == 6):
+        return "BB"
+    elif(rating  == 5):
+        return "B"
+    elif(rating  == 4):
+        return "CCC"
+    elif(rating  == 3):
+        return "CC"
+    elif(rating  == 2):
+        return "C"
+    elif(rating  == 1):
+        return "D"
+    elif(rating  == 0):
+        return "SUB D"
+
+def rateFromDict(dict):
+    totalRatio = dict['totalAssets'] / dict['totalLiabilities']
+    currentRatio = dict['currentAssets'] / dict['currentLiabilities']
+    equityAssetRatio = (dict['shareholderEquity'] + dict['longTermDebt'] ) / dict['fixedAssets']
+    timesInterestEarned = dict['earningPreInterestTax'] / dict['interestExpense']
+    incomeCapexRatio = (dict['netIncome'] + dict['depreciation']) / (dict['capEx'] + dict['inventoryChange'])
+    debtIncomeRatio = dict['totalDebt'] / dict['netIncome']
+    expenseSalesRatio = dict['operatingExpense'] / dict['sales']
+    arr = [totalRatio, currentRatio, equityAssetRatio, dict['equityReturn'],
+        timesInterestEarned, incomeCapexRatio, debtIncomeRatio, expenseSalesRatio]
+    return rate(arr)
+
 if __name__ == '__main__':
-    print(rate(test_inputs.in_data[0]))
+    for input in test_inputs.in_data:
+        print("""Company: {0}, Rating: {1}""".format(input['company_name'], rateFromDict(input)))
