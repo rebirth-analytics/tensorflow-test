56d5992d2b04ced702846499e6e7eabdc39c3567
glenn <glenn@glenns-MacBook.local>
2019-01-13 23:22:49 -0500

    remove yahoofinancial usage for performance reasons

--- lib/nn_predict.py
+++ lib/nn_predict.py
@@ -9,6 +9,7 @@ companies_df = pd.read_csv('lib/secwiki_tickers.csv')
 export_dir = './tmp/'
 checkpoint_path = tf.train.latest_checkpoint(export_dir)
 saver = tf.train.import_meta_graph(checkpoint_path + ".meta", import_scope=None)
+rating_cache = {}
 
 def load_graph():
     ret_list = []
@@ -62,42 +63,59 @@ def RepresentsInt(s):
         return False
 
 def getRatingFor(symbol):
-    from yahoofinancials import YahooFinancials
-    yahoo_financials = YahooFinancials(symbol)
+    if symbol in rating_cache:
+        return rating_cache[symbol]
+
     import requests
     data_dict = {}
     scaling_factor = 1000
-    yahoo_url="https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?modules=financialData".format(symbol)
-    fin_stats_dict = requests.get(yahoo_url).json()
-    x_int = lambda s: 0 if s is None else int(s) / scaling_factor
-    data_dict['totalDebt'] = int(fin_stats_dict['quoteSummary']['result'][0]['financialData']['totalDebt']['raw'] / scaling_factor)
-    bal_history = yahoo_financials.get_financial_stmts('quarterly', 'balance')['balanceSheetHistoryQuarterly'][symbol]
-    balance_dict = list(bal_history[0].values())[0]
-    cash_dict = list(yahoo_financials.get_financial_stmts('quarterly', 'cash')['cashflowStatementHistoryQuarterly'][symbol][0].values())[0]
-    data_dict['inventoryChange'] = 0
-    if 'inventory' in balance_dict:
-        prev_balance_dict = list(bal_history[1].values())[0]
-        if 'inventory' in prev_balance_dict:
-            data_dict['inventoryChange'] = x_int(balance_dict['inventory']) - x_int(prev_balance_dict['inventory'])
-    
-    data_dict['interestExpense'] = x_int(yahoo_financials.get_interest_expense()) 
-    data_dict['netIncome'] = x_int(yahoo_financials.get_net_income()) 
-    data_dict['totalLiabilities'] = x_int(balance_dict['totalLiab']) 
-    data_dict['currentLiabilities'] = x_int(balance_dict['totalLiab']) 
-    data_dict['currentAssets'] = x_int(balance_dict['totalCurrentAssets']) 
-    data_dict['totalAssets'] = x_int(balance_dict['totalAssets']) 
-    data_dict['fixedAssets'] = x_int(balance_dict['propertyPlantEquipment']) 
-    data_dict['shareholderEquity'] = x_int(balance_dict['totalStockholderEquity']) 
-    data_dict['longTermDebt'] = x_int(balance_dict['longTermDebt']) 
-    data_dict['operatingExpense'] = x_int(yahoo_financials.get_total_operating_expense()) 
-    data_dict['sales'] = x_int(yahoo_financials.get_total_revenue()) 
-    data_dict['earningPreInterestTax'] = x_int(yahoo_financials.get_income_before_tax()) + data_dict['interestExpense']
-    data_dict['netIncome'] = x_int(yahoo_financials.get_net_income()) 
-    data_dict['depreciation'] = x_int(cash_dict['depreciation']) 
-    data_dict['capEx'] = x_int(cash_dict['capitalExpenditures']) 
-    data_dict['equityReturn'] = float(data_dict['netIncome'] / data_dict['shareholderEquity'])
+    yahoo_fin_url="https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?modules=financialData".format(symbol)
+    fin_dict = requests.get(yahoo_fin_url).json()
+    if fin_dict['quoteSummary']['error'] is not None:
+        return -1
 
-    return rateFromDict(data_dict)
+    fin_stats_dict = fin_dict['quoteSummary']['result'][0]['financialData']
+    yahoo_income_url="https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?modules=incomeStatementHistory".format(symbol)
+    income_dict = requests.get(yahoo_income_url).json()['quoteSummary']['result'][0]['incomeStatementHistory']['incomeStatementHistory'][0]
+    yahoo_balance_url="https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?modules=balanceSheetHistory".format(symbol)
+    b_history = requests.get(yahoo_balance_url).json()['quoteSummary']['result'][0]['balanceSheetHistory']['balanceSheetStatements']
+    b_dict = b_history[0]
+    yahoo_cash_url="https://query2.finance.yahoo.com/v10/finance/quoteSummary/{0}?modules=cashflowStatementHistory".format(symbol)
+    cash_dict = requests.get(yahoo_cash_url).json()['quoteSummary']['result'][0]['cashflowStatementHistory']['cashflowStatements'][0]
+    x_int = lambda d, k: 0 if k not in d or d[k] is None else int(d[k]) / scaling_factor
+    data_dict['totalDebt'] = int(fin_stats_dict['totalDebt']['raw'] / scaling_factor)
+    data_dict['totalLiabilities'] = int(b_dict['totalLiab']['raw']) / scaling_factor
+    data_dict['currentLiabilities'] = x_int(b_dict['totalCurrentLiabilities'],'raw')
+    data_dict['currentAssets'] = int(b_dict['totalCurrentAssets']['raw']) / scaling_factor
+    data_dict['totalAssets'] = x_int(b_dict['totalAssets'], 'raw') 
+    data_dict['fixedAssets'] = x_int(b_dict['propertyPlantEquipment'], 'raw')
+    data_dict['shareholderEquity'] = x_int(b_dict['totalStockholderEquity'], 'raw') 
+    data_dict['longTermDebt'] = x_int(b_dict['longTermDebt'], 'raw') 
+    data_dict['interestExpense'] = x_int(income_dict['interestExpense'],'raw')
+    data_dict['netIncome'] = int(income_dict['netIncome']['raw'] / scaling_factor)
+    data_dict['operatingExpense'] = int(income_dict['totalOperatingExpenses']['raw'] / scaling_factor)
+    data_dict['sales'] = int(income_dict['totalRevenue']['raw'] / scaling_factor)
+    data_dict['earningPreInterestTax'] = int(income_dict['incomeBeforeTax']['raw'] / scaling_factor) + data_dict['interestExpense']
+    data_dict['depreciation'] = x_int(cash_dict['depreciation'], 'raw')
+    data_dict['capEx'] = x_int(cash_dict['capitalExpenditures'], 'raw') 
+    data_dict['inventoryChange'] = 0
+    if 'inventory' in b_dict and 'raw' in b_dict['inventory']:
+        prev_b_dict = b_history[1]
+        if 'inventory' in prev_b_dict and 'raw' in prev_b_dict['inventory']:
+            data_dict['inventoryChange'] = x_int(b_dict['inventory'], 'raw') - x_int(prev_b_dict['inventory'], 'raw')
+    
+    if 'raw' not in fin_stats_dict['currentRatio']:
+        data_dict['currentRatio'] = float(data_dict['currentAssets'] / data_dict['currentLiabilities'])
+    else:
+        data_dict['currentRatio'] = float(fin_stats_dict['currentRatio']['raw'])
+
+    if 'raw' not in fin_stats_dict['returnOnEquity']:
+        data_dict['equityReturn'] = float(data_dict['netIncome'] / data_dict['shareholderEquity'])
+    else:
+        data_dict['equityReturn'] = float(fin_stats_dict['returnOnEquity']['raw'])
+
+    rating_cache[symbol] = rateFromDict(data_dict)
+    return rating_cache[symbol]
 
 def rateFromDict(dict):
     int_expense = dict['interestExpense']
@@ -113,12 +131,13 @@ def rateFromDict(dict):
         expenseSalesRatio = dict['operatingExpense'] / dict['sales']
 
     totalRatio = dict['totalAssets'] / dict['totalLiabilities']
-    currentRatio = dict['currentAssets'] / dict['currentLiabilities']
+    if 'currentRatio' not in dict:
+        dict['currentRatio'] = dict['currentAssets'] / dict['currentLiabilities']
     timesInterestEarned = dict['earningPreInterestTax'] / int_expense
     incomeCapexRatio = (dict['netIncome'] + dict['depreciation']) / (dict['capEx'] + dict['inventoryChange'])
     debtIncomeRatio = dict['totalDebt'] / dict['netIncome']
     expenseSalesRatio = dict['operatingExpense'] / dict['sales']
-    arr = [totalRatio, currentRatio, equityAssetRatio, dict['equityReturn'],
+    arr = [totalRatio, dict['currentRatio'], equityAssetRatio, dict['equityReturn'],
         timesInterestEarned, incomeCapexRatio, debtIncomeRatio, expenseSalesRatio]
     return rate(arr)
 
@@ -131,4 +150,14 @@ def getCompanyName(symbol):
 if __name__ == '__main__':
     #for input in test_inputs.in_data:
         #print("""Company: {0}, Rating: {1}""".format(input['company_name'], rateFromDict(input)))
-    print(getRatingFor('NFLX'))
+    #this_rating = int(getRatingFor('NFLX'))
+    this_symbol = ""
+    for i, row in companies_df.iterrows():
+        this_symbol = row['Ticker']
+        if row['Rating'] == -1:
+            try: 
+                rating = int(getRatingFor(this_symbol))
+                companies_df.at[i,'Rating'] = rating
+            except : 
+                pass
+    companies_df.to_csv('public_ratings.csv')
